c35c29aa35f3c0357f8f7978b1afd1713cd5e232
Nabeel KM <nabeelkahlil403@gmail.com>
2023-12-02 14:21:34 +0700

    create html page

--- __main__.py
+++ __main__.py
@@ -1,18 +1,32 @@
 import os
 import pathlib
-from weasyprint import HTML, CSS
+from css_inline import inline
 
-from convert import md_to_html
+import convert
+
+path = pathlib.Path(__file__)
+module_path = path.parent.resolve()
+
+
+def build_html_file(html):
+    main_style_path = os.path.join(module_path, 'styles', 'pdf.css')
+    web_extra = os.path.join(module_path, 'styles', 'web.css')
+
+    css = open(main_style_path, 'r').read()
+    css += open(web_extra, 'r').read()
+
+    compiled = inline(html, extra_css=css)
+
+    with open(os.path.join(module_path, 'temp', 'resumt-html.html'), 'w') as file:
+        file.write(compiled)
 
 
 def app():
-    path = pathlib.Path(__file__)
-    md_path = os.path.join(path.parent.resolve(), 'resume.md')
-    html_str = md_to_html(md_path)
+    md_path = os.path.join(module_path, 'resume.md')
+    html = convert.md_to_html(md_path)
 
-    html = HTML(string=html_str)
-    css = CSS(filename='./styles/pdf.css')
-    html.write_pdf('./temp/resume.pdf', stylesheets=[css])
+    build_html_file(html)
+    convert.html_to_pdf(html)
 
 
 if __name__ == '__main__':
--- convert.py
+++ convert.py
@@ -1,8 +1,17 @@
 import subprocess
 
+from weasyprint import HTML, CSS
+
 
 def md_to_html(filepath):
     command = f'pandoc --from gfm -t html {filepath}'
     result = subprocess.run(command.split(' '), capture_output=True)
-    return result.stdout
+    html = result.stdout.decode()
+    return f"<body>{html}</body>"
+
+
+def html_to_pdf(html_str):
+    html = HTML(string=html_str)
+    css = CSS(filename='./styles/pdf.css')
+    html.write_pdf('./temp/resume.pdf', stylesheets=[css])
     
